ea345d978862aad288d00ee25c6ae003d1ae8b5d
Kyosuke Fujimoto <31386431+lusingander@users.noreply.github.com>
2024-09-02 08:39:47 +0900

    Remove cache (#39)
    * Remove cache module
    
    * Remove --no-cache option
    
    * Remove unnecessary dependencies

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,5 @@
-mod cache;
 mod calc;
 mod image;
 
-pub use cache::*;
 pub use calc::*;
 pub use image::*;
--- src/lib.rs
+++ src/lib.rs
@@ -33,10 +33,6 @@ struct Args {
     /// Preload all graph images
     #[arg(long, default_value = "false")]
     preload: bool,
-
-    /// Do not use graph image cache
-    #[arg(long, default_value = "false")]
-    no_cache: bool,
 }
 
 #[derive(Debug, Clone, ValueEnum)]
@@ -84,7 +80,7 @@ pub fn run() -> std::io::Result<()> {
 
     let graph = graph::calc_graph(&repository);
 
-    let graph_image_options = graph::GraphImageOptions::new(color_set.clone(), args.no_cache);
+    let graph_image_options = graph::GraphImageOptions::new(color_set.clone());
     let graph_image_manager =
         GraphImageManager::new(&graph, graph_image_options, image_protocol, args.preload);
 
